340ddef3165a4781eef14002412e5b5fb0cce675
Tien Do <tiendo220199@gmail.com>
2020-08-25 15:14:03 +0700

    Router

---
+++ src/components/Cart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Cart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>Cart</div>
+        );
+    }
+}
+
+export default Cart;
---
+++ src/components/Menu.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem
+} from 'reactstrap';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Cart from './Cart'
+
+class Menu extends React.Component {
+
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar color="light" light expand="sm">
+            <NavbarBrand>Cart Project</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="ml-4">
+                <Link to="/">Home</Link>
+              </NavItem>
+              <NavItem className="ml-4">
+                <Link to="/cart">Cart (0)</Link>
+              </NavItem>
+            </Nav>
+          </Navbar>
+
+          <Switch>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default Menu;
