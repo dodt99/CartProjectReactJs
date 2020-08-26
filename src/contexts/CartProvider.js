c37773d05acb2d34e4f9aae6b5c674346641eae2
Tien Do <tiendo220199@gmail.com>
2020-08-26 16:15:22 +0700

    done

--- src/contexts/CartProvider.js
+++ src/contexts/CartProvider.js
@@ -13,10 +13,18 @@ export class CartProvider extends Component {
     this.addToCart = this.addToCart.bind(this);
   }
 
+  componentDidMount() {
+    let data = JSON.parse(localStorage.getItem('products'));
+    this.setState({
+      products: (data != null) ? data : []
+    });
+  }
+
   addToCart(product) {
     this.setState({
       products: this.state.products.concat(product)
     })
+    localStorage.setItem('products', JSON.stringify(this.state.products));
     console.log(this.state.products);
   }
 
